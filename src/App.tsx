b9288aaecb5e6be0375234ed6fefad56ca185be2
SeungHun333 <yui332@naver.com>
2025-05-09 07:41:47 +0900

    회원가입, 로그인 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,16 @@
-import React, { useState } from "react";
-import { useTabStore } from "./stores/useTabStore";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import HomePage from "./routes/HomePage";
-import MovieDetailPage from "./routes/MovieDetailPage";
-import CommunityPage from "./routes/CommunityPage";
-import PostDetailPage from "./routes/PostDetailPage";
-import PostCreatePage from "./routes/PostCreatePage";
-import PostEditPage from "./routes/PostEditPage";
-import LoginPage from "./routes/LoginPage";
-import SignupPage from "./routes/SignupPage";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "../src/routes/LoginPage";
+import SignupPage from "../src/routes/SignupPage";
+import PostListPage from "../src/routes/HomePage";
 
-const App: React.FC = () => {
-  const activeTab = useTabStore((state) => state.activeTab);
-  const setActiveTab = useTabStore((state) => state.setActiveTab);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-    setActiveTab("home");
-  };
-
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-    setActiveTab("home");
-  };
-
+function App() {
   return (
-    <div className="min-h-screen bg-gray-900 text-white">
-      <Header isLoggedIn={isLoggedIn} onLogout={handleLogout} />
-      <main className="container mx-auto px-4 pt-24 pb-12 min-h-[1024px]">
-        <div className="max-w-7xl mx-auto">
-        {activeTab === "home" && <HomePage />}
-          {activeTab === "movie-detail" && <MovieDetailPage />}
-          {activeTab === "community" && <CommunityPage />}
-          {activeTab === "post-detail" && <PostDetailPage />}
-          {activeTab === "create-post" && <PostCreatePage />}
-          {activeTab === "edit-post" && <PostEditPage />}
-          {activeTab === "login" && <LoginPage />}
-          {activeTab === "signup" && <SignupPage />}
-        </div>
-      </main>
-      <Footer />
-    </div>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/posts" element={<PostListPage />} />
+    </Routes>
   );
-};
+}
 
 export default App;
