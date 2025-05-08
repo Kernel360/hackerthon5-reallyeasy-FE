77e137f0104508b7f3b9f4a6b1dfd2031a62036c
SeungHun333 <yui332@naver.com>
2025-05-09 08:44:46 +0900

    vite config 파일 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }]
   },
   server: {
+    port : 3000,
     proxy: {
       '/api': {
-        // target: 'http://3.39.56.196:8080',
-        target: 'http://localhost:8080',
+        target: 'http://3.39.56.196:8080',
         changeOrigin: true
       }
     }
